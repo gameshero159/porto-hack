f3db6077c7ca79ee40965439ccb7ced1079541fe
Cauê Vidal Moura <caue_caua@hotmail.com>
2022-07-30 20:26:19 -0300

    feat!: first commit
    Signed-off-by: Cauê Vidal Moura <caue_caua@hotmail.com>

---
+++ src/main/java/br/com/portotech/portotech/PortotechApplication.java
@@ -0,0 +1,13 @@
+package br.com.portotech.portotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortotechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/portotech/portotech/PortotechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portotech.portotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
