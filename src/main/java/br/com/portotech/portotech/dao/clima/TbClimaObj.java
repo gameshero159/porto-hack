852927e8d82f97c20c9443e813f64bbdf1c1c6c5
Cauê Vidal Moura <caue_caua@hotmail.com>
2022-07-31 00:58:30 -0300

    feat: adicionado a tabela de clima
    Signed-off-by: Cauê Vidal Moura <caue_caua@hotmail.com>

---
+++ src/main/java/br/com/portotech/portotech/dao/clima/TbClimaObj.java
@@ -0,0 +1,42 @@
+package br.com.portotech.portotech.dao.clima;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_clima", schema = "portotech")
+public class TbClimaObj {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cd_clima")
+    private Integer cdClima;
+
+    @Column(name = "tx_cidade")
+    private String txCidade;
+
+    @Column(name = "tx_uf")
+    private String txUf;
+
+    @Column(name = "tx_pais")
+    private String txPais;
+
+    @Column(name = "vl_tempatura")
+    private Double vlTemperatura;
+
+    @Column(name = "tx_direcao_vento")
+    private String txDirecaoVento;
+
+    @Column(name = "vl_velocidade_vento")
+    private Double vlVelocidadeVento;
+
+    @Column(name = "tx_condicao")
+    private String txCondicao;
+
+    @Column(name = "dt_consulta")
+    private Date dtConsulta;
+}
---
+++ src/main/java/br/com/portotech/portotech/dao/clima/repository/TbClimaRepository.java
@@ -0,0 +1,13 @@
+package br.com.portotech.portotech.dao.clima.repository;
+
+import br.com.portotech.portotech.dao.clima.TbClimaObj;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface TbClimaRepository extends JpaRepository<TbClimaObj, Integer> {
+    List<TbClimaObj> findByDtConsultaBetween(Date dtInicial, Date dtFinal);
+}
