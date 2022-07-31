6bf3ff6634016ed18dbf1336654bba91592cd7fa
Cauê Vidal Moura <caue_caua@hotmail.com>
2022-07-31 11:32:58 -0300

    refactor!: removido algumas váriaveis desnecessárias. fix: corrigido falha no clima
    Signed-off-by: Cauê Vidal Moura <caue_caua@hotmail.com>

--- src/main/java/br/com/portotech/portotech/modules/dashboard/controller/DashboardController.java
+++ src/main/java/br/com/portotech/portotech/modules/dashboard/controller/DashboardController.java
@@ -1,8 +1,5 @@
 package br.com.portotech.portotech.modules.dashboard.controller;
 
-import br.com.portotech.portotech.dao.clima.TbClimaObj;
-import br.com.portotech.portotech.dao.portosantos.TbProgramadosPortoSantosObj;
-import br.com.portotech.portotech.dao.tabuamare.TbTabuaMareObj;
 import br.com.portotech.portotech.modules.clima.service.TbClimaService;
 import br.com.portotech.portotech.modules.programadosportosantos.service.TbProgramadosPortoSantosService;
 import br.com.portotech.portotech.modules.tabuamare.service.TbTabuaMareService;
@@ -10,8 +7,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @AllArgsConstructor
 @Controller
@@ -26,14 +21,11 @@ public class DashboardController {
     @GetMapping("/dashboard")
     public ModelAndView show(){
         ModelAndView mv = new ModelAndView();
-        List<TbProgramadosPortoSantosObj> tbProgramadosPortoSantosObj = tbProgramadosPortoSantosService.listagemPortoSantosAtual();
-        List<TbTabuaMareObj> tbTabuaMareObj = tbTabuaMareService.picoMareAtual();
-        TbClimaObj tbClimaObj = tbClimaService.climaAtual();
 
-        mv.addObject("lstProgramados", tbProgramadosPortoSantosObj);
-        mv.addObject("tbTabuaMareObj", tbTabuaMareObj);
-        mv.addObject("tbClima", tbClimaObj);
-        mv.addObject("tbClimaFuturo", tbClimaObj);
+        mv.addObject("lstProgramados", tbProgramadosPortoSantosService.listagemPortoSantosAtual());
+        mv.addObject("tbTabuaMareObj", tbTabuaMareService.picoMareAtual());
+        mv.addObject("tbClima", tbClimaService.climaAtual());
+        mv.addObject("tbClimaFuturo", tbClimaService.climaFuturo());
 
         return mv;
     }
