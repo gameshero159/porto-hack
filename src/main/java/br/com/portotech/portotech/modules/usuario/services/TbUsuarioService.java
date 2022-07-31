a199c0a47296682fe30ee7d2c890ea0bcf7a950a
Cauê Vidal Moura <caue_caua@hotmail.com>
2022-07-31 11:30:00 -0300

    refactor: removido o cadastro de usuário
    Signed-off-by: Cauê Vidal Moura <caue_caua@hotmail.com>

--- src/main/java/br/com/portotech/portotech/modules/usuario/services/TbUsuarioService.java
+++ src/main/java/br/com/portotech/portotech/modules/usuario/services/TbUsuarioService.java
@@ -6,37 +6,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class TbUsuarioService implements UserDetailsService {
     private final TbUsuarioRepository tbUsuarioRepository;
-
-    private final PasswordEncoder passwordEncoder;
-
-    public TbUsuarioObj createUser(TbUsuarioObj tbUsuarioObj) {
-        if (tbUsuarioObj.getCdUsuario() == null) {
-            Optional<TbUsuarioObj> optionalTbUsuarioObj = tbUsuarioRepository.findByTxEmail(tbUsuarioObj.getTxEmail());
-
-            if (optionalTbUsuarioObj.isPresent()) {
-                return null;
-            }
-
-            tbUsuarioObj.setCkAtivo(2);
-            tbUsuarioObj.setDtCriacao(new Date());
-        }
-
-        if (tbUsuarioObj.getTxSenha() != null && !tbUsuarioObj.getTxSenha().startsWith("$2a$")) {
-            tbUsuarioObj.setTxSenha(passwordEncoder.encode(tbUsuarioObj.getTxSenha()));
-        }
-
-        return tbUsuarioRepository.save(tbUsuarioObj);
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
