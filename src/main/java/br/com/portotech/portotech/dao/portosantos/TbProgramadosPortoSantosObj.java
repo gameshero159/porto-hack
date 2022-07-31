e411f59759ea34b46218ce9d6211557ffb93456d
RubensJTP <rubensneto@jtpsolution.com.br>
2022-07-30 23:36:45 -0300

    chore: layout default e inicio dashboard

---
+++ src/main/java/br/com/portotech/portotech/dao/portosantos/TbProgramadosPortoSantosObj.java
@@ -0,0 +1,45 @@
+package br.com.portotech.portotech.dao.portosantos;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_programados_porto_santos", schema = "portotech", catalog = "")
+public class TbProgramadosPortoSantosObj {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "cd_programados_porto_santos", nullable = false)
+    private int cdProgramadosPortoSantos;
+    
+    @Column(name = "dt_prev_inicio", nullable = true)
+    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private Date dtPrevInicio;
+    
+    @Column(name = "dt_prev_fim", nullable = true)
+    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private Date dtPrevFim;
+    
+    @Column(name = "tx_berco", nullable = true, length = 45)
+    private String txBerco;
+    
+    @Column(name = "tx_navio", nullable = true, length = 45)
+    private String txNavio;
+    
+    @Column(name = "tx_tipo_carga", nullable = true, length = 45)
+    private String txTipoCarga;
+    
+    @Column(name = "tx_evento", nullable = true, length = 45)
+    private String txEvento;
+    
+    @Column(name = "tx_ref_viagem", nullable = true, length = 45)
+    private String txRefViagem;
+
+    @Column(name = "cd_duv", nullable = true)
+    private Integer cdDuv;
+
+}
